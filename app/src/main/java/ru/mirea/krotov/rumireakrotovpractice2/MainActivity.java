dfbbe2aa5d36d0e069b0aa97c59151a00d8b847d
IWANTP1ZZA <71212366+IWANTP1ZZA@users.noreply.github.com>
2021-06-01 15:24:42 +0300

    Initial commit

---
+++ app/src/main/java/ru/mirea/krotov/rumireakrotovpractice2/MainActivity.java
@@ -0,0 +1,17 @@
+package ru.mirea.krotov.rumireakrotovpractice2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mirea.krotov.rumireakrotovpractice2.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
